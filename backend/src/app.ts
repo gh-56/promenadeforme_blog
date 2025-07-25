18f5e035fd4696ebdf12d67385996ac9c9fbeae9
gh-56 <erser0506@gmail.com>
2025-07-26 02:08:57 +0900

    feat: 블로그 프로젝트 초기 설정 및 프론트엔드, 백엔드 rest 통신 (cors 설정 완료)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,18 @@ import cors from 'cors';
 
 const app = express();
 
+app.set('port', process.env.PORT || 4000);
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Promenadeforme 블로그 백엔드 API');
+app.get('/api', (req, res) => {
+  res.json({ code: 200, message: 'Promenadeforme 블로그 백엔드 API' });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`서버가 ${app.get('port')}번 포트에서 실행 중입니다.`);
+  console.log(`http://localhost:${app.get('port')}`);
 });
 
 export default app;
--- backend/src/config/index.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const config = {
-  port: process.env.PORT || 5000,
-  mongoUri: process.env.MONGO_URI || '',
-  jwtSecret: process.env.JWT_SECRET || 'jwtsecret',
-};
