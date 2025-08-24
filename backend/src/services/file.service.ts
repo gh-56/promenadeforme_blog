ba42e0962e204e7dabdd6a8391e6fb1a6a5273d0
gh-56 <dinggul56@gmail.com>
2025-08-25 04:02:55 +0900

    fix 기본 이미지 참조수 마이너스로 떨어지는 현상 수정

--- backend/src/services/file.service.ts
+++ backend/src/services/file.service.ts
@@ -9,15 +9,8 @@ export interface File {
   originalname: string;
 }
 
-export const saveImageFile = async (
-  file: File,
-  userId: string,
-  isUpdated?: boolean
-) => {
-  const fileHash = crypto
-    .createHash('sha256')
-    .update(file.buffer)
-    .digest('hex');
+export const saveImageFile = async (file: File, userId: string, isUpdated?: boolean) => {
+  const fileHash = crypto.createHash('sha256').update(file.buffer).digest('hex');
   const fileExt = path.extname(file.originalname);
   const newFileName = `${fileHash}${fileExt}`;
   const savePath = path.resolve(process.cwd(), 'uploads', newFileName);
@@ -57,7 +50,12 @@ export const deleteImageFile = async (imageIds: Types.ObjectId[]) => {
         console.warn(`${imageId}번 이미지가 없습니다.`);
         continue;
       } else {
-        image.referenceCount -= 1;
+        if (image.url === 'http://localhost:4000/images/default-post-image.jpg') {
+          image.referenceCount = 1;
+        } else {
+          image.referenceCount -= 1;
+        }
+
         await image.save();
 
         if (image.referenceCount <= 0) {
@@ -68,10 +66,7 @@ export const deleteImageFile = async (imageIds: Types.ObjectId[]) => {
             await Image.findOneAndDelete({ _id: imageId });
             console.log(`${imageId}번 이미지가 삭제되었습니다.`);
           } catch (fileError) {
-            console.error(
-              `${imageId}번 이미지 삭제에 실패했습니다.`,
-              fileError
-            );
+            console.error(`${imageId}번 이미지 삭제에 실패했습니다.`, fileError);
           }
         }
       }
