9858378fcf6b0623d2c65e5655cfea2e67c6f003
gh-56 <dinggul56@gmail.com>
2025-09-26 05:08:04 +0900

    feat: 미리보기 프론트에서 백으로 이동 구현

--- backend/src/schemas/post.ts
+++ backend/src/schemas/post.ts
@@ -1,4 +1,35 @@
 import { Schema, model } from 'mongoose';
+import Image from './image.js';
+
+const generateSummary = (contentJson: string) => {
+  if (!contentJson) return '';
+
+  let textContent = '';
+  try {
+    const parsedContent = JSON.parse(contentJson);
+
+    if (parsedContent.content) {
+      for (const block of parsedContent.content) {
+        if (block.content) {
+          for (const item of block.content) {
+            if (item.type === 'text' && item.text) {
+              textContent += item.text + ' ';
+            }
+          }
+        }
+        if (textContent.length > 150) break;
+      }
+    }
+  } catch (error) {
+    console.error('콘텐츠 파싱 실패 (요약 생성):', error);
+    return '내용을 불러올 수 없습니다.';
+  }
+
+  // 150자 이내로 자르고, 끝에 ...을 붙입니다.
+  return (
+    textContent.trim().slice(0, 150) + (textContent.length > 150 ? '...' : '')
+  );
+};
 
 const postSchema = new Schema({
   title: {
@@ -8,6 +39,9 @@ const postSchema = new Schema({
   content: {
     type: String,
     required: true,
+  },
+  summary: {
+    type: String,
   },
   category: {
     type: Schema.Types.ObjectId,
@@ -26,6 +60,9 @@ const postSchema = new Schema({
       required: false,
     },
   ],
+  thumbnail: {
+    type: String,
+  },
   tags: [
     {
       type: String,
@@ -58,6 +95,32 @@ postSchema.index(
     partialFilterExpression: { status: 'draft', expireAt: { $type: 'date' } },
   },
 );
+
+postSchema.pre('save', async function (next) {
+  // 게시글 미리보기 텍스트
+  if (this.isModified('content')) {
+    this.summary = generateSummary(this.content);
+  }
+
+  // 썸네일
+  if (this.isModified('images')) {
+    if (this.images && this.images.length > 0) {
+      const firstImageId = this.images[0];
+
+      const imageDoc = await Image.findById(firstImageId);
+
+      if (imageDoc) {
+        this.thumbnail = imageDoc.url;
+      } else {
+        this.thumbnail = `${process.env.GCLOUD_STORAGE_IMAGE_URL}/${process.env.GCLOUD_STORAGE_BUCKET}/default-post-image.webp`;
+      }
+    } else {
+      this.thumbnail = `${process.env.GCLOUD_STORAGE_IMAGE_URL}/${process.env.GCLOUD_STORAGE_BUCKET}/default-post-image.webp`;
+    }
+  }
+
+  next();
+});
 
 const Post = model('Post', postSchema);
 
